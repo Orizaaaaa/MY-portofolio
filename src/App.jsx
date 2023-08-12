c40446a39cfbe922581ebcf5a7700ef1268af545
oryzaaa <oryzasativacikal@gmail.com>
2023-08-12 16:09:54 +0700

    Revisi Portofolio

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <nav className="navbar navbar-expand-lg bg-none">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#"> <b>PORTOFOLIO</b><br /> <p>Informatics Student | Front End Developer</p> </a>
+          <span className="portofolio"> <b>PORTOFOLIO</b><br /> <p>Informatics Student | Front End Developer</p> </span>
 
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
