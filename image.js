56f96a498b3bcb4fa7efc4dc49a321c8d3ce4c90
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2023-09-09 10:22:00 +0700

    add about me

--- image.js
+++ image.js
@@ -1,5 +1,7 @@
 import profile from './src/assets/profile.svg'
+import aboutImage from './src/assets/aboutMe.svg'
 
 export {
-    profile
+    profile,
+    aboutImage
 }
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,4 +1,4 @@
-import { profile } from "../../image"
+import { aboutImage, profile } from "../../image"
 import Navbar from "../components/Fragment/Navbar"
 
 
@@ -22,6 +22,44 @@ function App() {
               <img src={profile} alt="profile" />
             </div>
           </div>
+        </section>
+
+        <section id="aboutMe" >
+          <h1 className="text-center text-light" >About Me</h1>
+          <div className="row">
+            <div className="col">
+              <img src={aboutImage} alt="" />
+            </div>
+
+            <div className="col">
+              <div className="AboutMySelf">
+                <h4 className="text-light">Hello👋 i'am <span>Oriza Sativa Cikal.M</span> <br /></h4>
+                <h5 className="text-light"><span>A Frontend Developer</span> based  in <span>Indonesia</span></h5>
+                <p className="mt-4 text-light" >Hello everyone! I am Muhammad Sufyan, a frontend mobile and website developer.
+                  I am a intermediate experienced and I hope with this I can always learning something new
+                  to be able to invent digital applications that can help many people in the future.
+                  Can work alone or in a team.</p>
+                <hr className="text-light" />
+                <div className="row text-light">
+                  <div className="col">
+                    <h4>1+ </h4>
+                    <h5>Years Of Experience</h5>
+                  </div>
+                  <div className="col">
+                    <h4>2+ </h4>
+                    <h5>Programming Language Used</h5>
+                  </div>
+                  <div className="col">
+                    <h4>8+ </h4>
+                    <h5>Successed Projects</h5>
+                  </div>
+                </div>
+              </div>
+
+
+            </div>
+          </div>
+
         </section>
       </main>
     </>
