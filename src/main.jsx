969acf22bce5db0baec495198eb4d4161109d5a6
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2023-10-01 16:19:40 +0700

    add css home

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pages/App.jsx'
 import './css/index.css'
+import './css/home.css'
+import './css/aboutme.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 
