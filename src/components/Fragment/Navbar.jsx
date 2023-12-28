e97708e3a7e89e77dee29d34017b62c01208c8ee
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2023-12-28 09:39:33 +0700

    fixing bug navbar

--- src/components/Fragment/Navbar.jsx
+++ src/components/Fragment/Navbar.jsx
@@ -14,7 +14,10 @@ function Navbar({ children }) {
     return (
         <nav className={`navbar navbar-expand-lg position-fixed px-3 py-2 w-100 rounded-0  ${navbarBg ? "navbarbgActive" : ""}`}>
             <div className="container-fluid ">
-                <span className="portofolio "> <b>PORTOFOLIO</b><br /> <p className='p-0 m-0' >Informatics Student | Front End Developer</p> </span>
+                <span className="portofolio ">
+                    <b>PORTOFOLIO</b>
+                    <p className='p-0 m-0 ' >Informatics Student | Front End Developer</p>
+                </span>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
