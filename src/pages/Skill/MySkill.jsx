10e2c71913f29e7a7691781b02b05352128592f6
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2023-12-31 09:24:08 +0700

    resposive tabel skill

--- src/pages/Skill/MySkill.jsx
+++ src/pages/Skill/MySkill.jsx
@@ -8,37 +8,49 @@ function MySkill() {
             <h1 className="text-center text-light mb-5" >My Skills</h1>
             <div className="row">
                 <div className="col-lg-9 ">
-                    <div className="card bg-primary  p-5">
-                        <div className="row row-cols-lg-6 gap-3 justify-content-center align-items-center ">
-                            <div className=" col list-skill d-flex flex-column justify-content-center align-items-center">
-                                <img src={HtmlIMage} alt="htmlimage" />
-                                <h6>HTML</h6>
-                                <p className='p-0 m-0 text text-secondary-emphasis' >Advanced</p>
+                    <div className="card bg-primary p-5">
+                        <div className="row justify-content-start">
+                            <div className="col-lg-3  col-md-3 col-sm-4 col-6 list-skill ">
+                                <div className="p-3 d-flex flex-column justify-content-center align-items-center">
+                                    <img src={HtmlIMage} alt="htmlimage" />
+                                    <h6>HTML</h6>
+                                    <p className='p-0 m-0 text text-secondary-emphasis' >Advanced</p>
+                                </div>
                             </div>
-                            <div className=" col list-skill d-flex flex-column justify-content-center align-items-center">
-                                <img src={HtmlIMage} alt="htmlimage" />
-                                <h6>HTML</h6>
-                                <p className='p-0 m-0 text text-secondary-emphasis' >Advanced</p>
+                            <div className=" col-lg-3  col-md-3 col-sm-4 col-6 list-skill ">
+                                <div className="p-3 d-flex flex-column justify-content-center align-items-center">
+                                    <img src={HtmlIMage} alt="htmlimage" />
+                                    <h6>HTML</h6>
+                                    <p className='p-0 m-0 text text-secondary-emphasis' >Advanced</p>
+                                </div>
                             </div>
-                            <div className=" col list-skill d-flex flex-column justify-content-center align-items-center">
-                                <img src={HtmlIMage} alt="htmlimage" />
-                                <h6>HTML</h6>
-                                <p className='p-0 m-0 text text-secondary-emphasis' >Advanced</p>
+                            <div className=" col-lg-3  col-md-3 col-sm-4 col-6 list-skill ">
+                                <div className="p-3 d-flex flex-column justify-content-center align-items-center">
+                                    <img src={HtmlIMage} alt="htmlimage" />
+                                    <h6>HTML</h6>
+                                    <p className='p-0 m-0 text text-secondary-emphasis' >Advanced</p>
+                                </div>
                             </div>
-                            <div className=" col list-skill d-flex flex-column justify-content-center align-items-center">
-                                <img src={HtmlIMage} alt="htmlimage" />
-                                <h6>HTML</h6>
-                                <p className='p-0 m-0 text text-secondary-emphasis' >Advanced</p>
+                            <div className=" col-lg-3  col-md-3 col-sm-4 col-6 list-skill ">
+                                <div className="p-3 d-flex flex-column justify-content-center align-items-center">
+                                    <img src={HtmlIMage} alt="htmlimage" />
+                                    <h6>HTML</h6>
+                                    <p className='p-0 m-0 text text-secondary-emphasis' >Advanced</p>
+                                </div>
                             </div>
-                            <div className=" col list-skill d-flex flex-column justify-content-center align-items-center">
-                                <img src={HtmlIMage} alt="htmlimage" />
-                                <h6>HTML</h6>
-                                <p className='p-0 m-0 text text-secondary-emphasis' >Advanced</p>
+                            <div className=" col-lg-3  col-md-3 col-sm-4 col-6 list-skill ">
+                                <div className="p-3 d-flex flex-column justify-content-center align-items-center">
+                                    <img src={HtmlIMage} alt="htmlimage" />
+                                    <h6>HTML</h6>
+                                    <p className='p-0 m-0 text text-secondary-emphasis' >Advanced</p>
+                                </div>
                             </div>
-                            <div className=" col list-skill d-flex flex-column justify-content-center align-items-center">
-                                <img src={HtmlIMage} alt="htmlimage" />
-                                <h6>HTML</h6>
-                                <p className='p-0 m-0 text text-secondary-emphasis' >Advanced</p>
+                            <div className=" col-lg-3  col-md-3 col-sm-4 col-6 list-skill ">
+                                <div className="p-3 d-flex flex-column justify-content-center align-items-center">
+                                    <img src={HtmlIMage} alt="htmlimage" />
+                                    <h6>HTML</h6>
+                                    <p className='p-0 m-0 text text-secondary-emphasis' >Advanced</p>
+                                </div>
                             </div>
 
                         </div>
